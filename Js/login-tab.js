828d81235b1f07f9ae587800dfada379a6ca8a82
NicolaasGeldenhuys <21100407@virtualwindow.co.za>
2021-10-04 23:27:56 +0200

    Progress Login further
    Progress for login almost completed

--- Js/login-tab.js
+++ Js/login-tab.js
@@ -2,52 +2,54 @@ $(function(){
 
     console.log("Document is ready.");
     
-    $(".camImg").hide();
+    
 
-    $(".left").hide();
+    $(".right").hide();
+    $(".left").show();
 
-    $(".btn").on("click", function(){
-        $(".panel").toggleClass("panel-slide");
 
-        $(".btn2").slideDown(1000);
+    // $(".btn").on("click", function(){
+    //     $(".panel").toggleClass("panel-slide");
 
-        $(".btn").slideUp(1000);
+    //     $(".btn2").slideDown(1000);
 
-        $(".loginIntro").slideUp(1000);
+    //     $(".btn").slideUp(1000);
 
-        $(".logoLogin").slideUp(1000);
+    //     $(".loginIntro").slideUp(1000);
 
-        $(".camImg").show(1000);
-    });
+    //     $(".logoLogin").slideUp(1000);
 
-    $(".btn").mouseenter(function(){
-        $(this).attr("src","img/LoginButton2.png");
-    });
+    //     $(".camImg").show(1000);
+    // });
 
-    $(".btn").mouseleave(function(){
-        $(this).attr("src","img/LoginButton.png");
-    });
+    // $(".btn").mouseenter(function(){
+    //     $(this).attr("src","img/LoginButton2.png");
+    // });
 
-    $(".btn2").on("click", function(){
-        $(".panel").toggleClass("panel-slide");
+    // $(".btn").mouseleave(function(){
+    //     $(this).attr("src","img/LoginButton.png");
+    // });
 
-        $(".btn2").slideUp(1000);
+    // $(".btn2").on("click", function(){
+    //     $(".panel").toggleClass("panel-slide");
 
-        $(".btn").slideDown(1000);
+    //     $(".btn2").slideUp(1000);
 
-        $(".loginIntro").slideDown(1000);
+    //     $(".btn").slideDown(1000);
 
-        $(".camImg").hide(1000);
+    //     $(".loginIntro").slideDown(1000);
 
-        $(".logoLogin").slideDown(1000);
-    });
+    //     $(".camImg").hide(1000);
 
-    $(".btn2").mouseenter(function(){
-        $(this).attr("src","img/SignUpButton2.png");
-    });
+    //     $(".logoLogin").slideDown(1000);
+    // });
 
-    $(".btn2").mouseleave(function(){
-        $(this).attr("src","img/SignUpButton.png");
-    });
+    // $(".btn2").mouseenter(function(){
+    //     $(this).attr("src","img/SignUpButton2.png");
+    // });
+
+    // $(".btn2").mouseleave(function(){
+    //     $(this).attr("src","img/SignUpButton.png");
+    // });
 
 });
