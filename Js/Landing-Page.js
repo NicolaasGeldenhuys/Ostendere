c01d1b161b16c4c96c6b4e0d03ae667704637668
Rudi <rudinaude@gmail.com>
2021-10-18 11:36:54 +0200

    API for Landing page
    trying to get the API to work with in the Landing page

--- Js/Landing-Page.js
+++ Js/Landing-Page.js
@@ -1,18 +1,79 @@
-var currentSlide = 1;
-var $slides = $('.slides');
-var slideCounter = $slides.children().length;
-var animateTime = 800;
-var slideTime = 5000;
+$(function() {
+    console.log('I see You');
 
-//console.log(slideCounter);
+    const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=20f38ff4b76767fa633118365643bf5d&language=en-US&page=1';
 
-setInterval(function() {
-    $slides.animate({ marginLeft: '-=900px' }, animateTime, function() {
-        currentSlide++;
-        if (currentSlide === slideCounter) {
-            currentSlide = 1;
-            $(this).css("margin-left", "0px")
-        }
+    $.getJSON(url, function(result) {
+        console.log(result);
+        for (i = 0; i < result.length; i++) {
+            console.log(result[i].page.results.id);
+            //console.log(result[i].page.results.title);
+            $(".slider").append(
+                "<ul class='slides'>\
+                     <li class='slide'><img src='" + result[i].backdrop_path + "' alt=''></li>\
+                     <li class='slide'><img src='' alt=''></li>\
+                     <li class='slide'><img src='' alt=''></li>\
+                     <li class='slide'><img src='' alt=''></li>\
+                     <li class='slide'><img src='' alt=''></li>\
+                     </ul>\
+                <div class='movieInfo'>\
+                    <h1>" + result[i].page.results.title + "</h1>\
+                     <p class='synopsis'>synopsis:" + result[i].overview + "</p>\
+                         <div class='btnContainer'>\
+                             <div class='btnWatchLater'>\
+                                 <h2>Watch Later</h2>\
+                            </div>\
+                             <!--btnWatchLater-->\
+                                <div class='btnWatchNow'>\
+                                     <h2>Watch Now</h2>\
+                                 </div>\
+                                    <!--btnWatchNow-->\
+                            </div>\
+                            <!--btnContainer-->\
+                </div>\
+                <!--movieInfo-->"
+            );
+            console.log("I have appended slider");
+
+
+
+        };
     });
-    console.log("going to change")
-}, slideTime);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var currentSlide = 1;
+    var $slides = $('.slides');
+    var slideCounter = $slides.children().length;
+    var animateTime = 800;
+    var slideTime = 5000;
+
+    //console.log(slideCounter);
+
+    setInterval(function() {
+        $slides.animate({ marginLeft: '-=900px' }, animateTime, function() {
+            currentSlide++;
+            if (currentSlide === slideCounter) {
+                currentSlide = 1;
+                $(this).css("margin-left", "0px")
+            }
+        });
+        console.log("going to change")
+    }, slideTime);
+
+});
