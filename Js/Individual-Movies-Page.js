b21081383b44f4ba1b40d124995033c0d7fd0fa5
Rudi <rudinaude@gmail.com>
2021-10-29 20:18:36 +0200

    In

--- Js/Individual-Movies-Page.js
+++ Js/Individual-Movies-Page.js
@@ -7,31 +7,36 @@ $(function() {
             return Math.floor(Math.random() * (min - max)) + min;
         }
 
-        for (i = 0; i <= getRndInteger(1, 0); i++) {
-            var backdrop = 'https://image.tmdb.org/t/p/w500/' + P_Movie.results[i].backdrop_path;
-            var poster = 'https://image.tmdb.org/t/p/w500/' + P_Movie.results[i].poster_path;
-            var TitleOfMovie = P_Movie.results[i].title;
-            var synopsisOfMovie = P_Movie.results[i].overview;
-            var rating = P_Movie.results[i].rating;
-            var year = P_Movie.results[i].year;
+        // for (i = 1; i < P_Movie.results.length; i++) {
+        var backdrop = 'https://image.tmdb.org/t/p/w500/' + P_Movie.results[1].backdrop_path;
+        var poster = 'https://image.tmdb.org/t/p/w500/' + P_Movie.results[1].poster_path;
+        var TitleOfMovie = P_Movie.results[1].title;
+        var synopsisOfMovie = P_Movie.results[1].overview;
+        var rating = P_Movie.results[1].rating;
+        var year = P_Movie.results[1].year;
 
-            $('.title-div').append(
-                " <div class='contentcon'></div>\
+        $('.firstRow').append(
+
+        );
+
+        $('.title-div').append(
+            " <div class='contentcon'></div>\
                     <h1 id='title'>" + TitleOfMovie + "</h1> "
-            );
+        );
 
 
-            $('.details').append(
-                "\
-                <h3 class='directors'> not Directors given </h3>\
-                <p class='contentD'>Text</p>\
+
+        $('.details').append(
+            "\
+                <h3 class='directors'> </h3>\
+                <p class='contentD'></p>\
                 <h3 class='trailers'>Trailer </h3>\
-                <h3 class='cast' style='margin-left: 384px; margin-top: -32px;'>No cast given </h3>\
-                <p class='contentC' style='margin-left: 15px; margin-top: -28px;'>Text</p>\
-                <h3 class='rating' style='margin-left: 437px; margin-top: -32px;'>" + rating + " </h3>\
+                <h3 class='cast' style='margin-left: 384px; margin-top: -32px;'> </h3>\
+                <p class='contentC' style='margin-left: 15px; margin-top: -28px;'></p>\
+                <h3 class='rating' style='margin-left: 437px; margin-top: -32px;'></h3>\
                 <div class='contentRdiv' style='margin-left: 15px; margin-top: -38px;'><img src='../img/img/icons/star_icon.svg' width='47.27px'><p class='contentR'>8.7</p></div>\
-                <h3 class='year' style='margin-left: 152px; margin-top: -32px;'>" + year + " </h3>\
-                <p class='contentY' style='margin-left: 15px; margin-top: -28px;'>Text</p>\
+                <h3 class='year' style='margin-left: 152px; margin-top: -32px;'> </h3>\
+                <p class='contentY' style='margin-left: 15px; margin-top: -28px;'></p>\
                 <h3 class='synopsis'>Synopsis </h3>\
                 <div class='desc'><p class='descTextc'>" + synopsisOfMovie + "</p></div>\
                 <img src='" + poster + "' class='mainTrailerButton' width='79px' height='24px'>\
@@ -45,10 +50,10 @@ $(function() {
                     <div class='buttonS4'></div>\
                     <div class='buttonS5'></div> \
                 "
-            );
+        );
 
 
-        }
+        // }
 
 
     });
