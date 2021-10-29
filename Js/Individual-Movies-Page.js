4ea647eb3dd8e76bc56f63610bf2d2dd7a30220d
Rudi <rudinaude@gmail.com>
2021-10-29 18:55:52 +0200

    broken

--- Js/Individual-Movies-Page.js
+++ Js/Individual-Movies-Page.js
@@ -14,31 +14,38 @@ $(function() {
             var synopsisOfMovie = P_Movie.results[i].overview;
             var rating = P_Movie.results[i].rating;
             var year = P_Movie.results[i].year;
-            // $('.details').append(
-            //     "\
-            //     <h3 class='directors'>Directors </h3>\
-            //     <p class='contentD'>Text</p>\
-            //     <h3 class='trailers'>Trailer </h3>\
-            //     <h3 class='cast' style='margin-left: 384px; margin-top: -32px;'>No cast given </h3>\
-            //     <p class='contentC' style='margin-left: 15px; margin-top: -28px;'>Text</p>\
-            //     <h3 class='rating' style='margin-left: 437px; margin-top: -32px;'>" + rating + " </h3>\
-            //     <div class='contentRdiv' style='margin-left: 15px; margin-top: -38px;'><img src='../img/img/icons/star_icon.svg' width='47.27px'><p class='contentR'>8.7</p></div>\
-            //     <h3 class='year' style='margin-left: 152px; margin-top: -32px;'>" + year + " </h3>\
-            //     <p class='contentY' style='margin-left: 15px; margin-top: -28px;'>Text</p>\
-            //     <h3 class='synopsis'>Synopsis </h3>\
-            //     <div class='desc'><p class='descTextc'>" + synopsisOfMovie + "</p></div>\
-            //     <img src='" + poster + "' class='mainTrailerButton' width='79px' height='24px'>\
-            //     <img src='" + backdrop + "' class='mainWatchLaterButton' width='79px' height='24px'>\
-            //     <div class='trailerVids'></div>\
-            //         <img src='../img/icons/playMain.svg'class='mainPlayButton' width='98px'>\
-            //         <div class='slider'></div>\
-            //         <div class='buttonS1'></div>\
-            //         <div class='buttonS2'></div>\
-            //         <div class='buttonS3'></div>\
-            //         <div class='buttonS4'></div>\
-            //         <div class='buttonS5'></div> \
-            //     "
-            // );
+
+            $('.title-div').append(
+                " <div class='contentcon'></div>\
+                    <h1 id='title'>" + TitleOfMovie + "</h1> "
+            );
+
+
+            $('.details').append(
+                "\
+                <h3 class='directors'> not Directors given </h3>\
+                <p class='contentD'>Text</p>\
+                <h3 class='trailers'>Trailer </h3>\
+                <h3 class='cast' style='margin-left: 384px; margin-top: -32px;'>No cast given </h3>\
+                <p class='contentC' style='margin-left: 15px; margin-top: -28px;'>Text</p>\
+                <h3 class='rating' style='margin-left: 437px; margin-top: -32px;'>" + rating + " </h3>\
+                <div class='contentRdiv' style='margin-left: 15px; margin-top: -38px;'><img src='../img/img/icons/star_icon.svg' width='47.27px'><p class='contentR'>8.7</p></div>\
+                <h3 class='year' style='margin-left: 152px; margin-top: -32px;'>" + year + " </h3>\
+                <p class='contentY' style='margin-left: 15px; margin-top: -28px;'>Text</p>\
+                <h3 class='synopsis'>Synopsis </h3>\
+                <div class='desc'><p class='descTextc'>" + synopsisOfMovie + "</p></div>\
+                <img src='" + poster + "' class='mainTrailerButton' width='79px' height='24px'>\
+                <img src='" + backdrop + "' class='mainWatchLaterButton' width='79px' height='24px'>\
+                <div class='trailerVids'></div>\
+                    <img src='../img/icons/playMain.svg'class='mainPlayButton' width='98px'>\
+                    <div class='slider'></div>\
+                    <div class='buttonS1'></div>\
+                    <div class='buttonS2'></div>\
+                    <div class='buttonS3'></div>\
+                    <div class='buttonS4'></div>\
+                    <div class='buttonS5'></div> \
+                "
+            );
 
 
         }
